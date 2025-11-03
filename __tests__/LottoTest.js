1fb526157b29bdc37a4abf939f351e95d7f75bdb
Kimd0ng <kdongh0406@gmail.com>
2025-11-03 15:22:55 +0900

    test(lotto): Lotto 추가 단위 테스트 케이스
    - 6개 미만/초과, 중복, 범위(0/46), 비정수(3.5) 예외 검증
    - 경계값(1, 45) 허용 검증
    - getNumbers() 방어적 복사 보장 검증

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,52 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
-  });
+describe('로또 클래스 테스트', () => {
+    test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 6, 7]);
+        }).toThrow('[ERROR]');
+    });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
+    test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 5]);
+        }).toThrow('[ERROR]');
+    });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    test('로또 번호의 개수가 6개 미만이면 예외가 발생한다.', () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5]);
+        }).toThrow('[ERROR]');
+    });
+
+    test.each([
+        [[0, 2, 3, 4, 5, 6]], // 0 포함
+        [[1, 2, 3, 4, 5, 46]], // 46 포함
+    ])('로또 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다. (%p)', (numbers) => {
+        expect(() => {
+            new Lotto(numbers);
+        }).toThrow('[ERROR]');
+    });
+
+    test('로또 번호에 정수가 아닌 값이 있으면 예외가 발생한다.', () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 3.5]);
+        }).toThrow('[ERROR]');
+    });
+
+    test('경계값(1, 45)은 허용된다.', () => {
+        expect(() => {
+            new Lotto([1, 2, 3, 4, 5, 45]);
+        }).not.toThrow();
+    });
+
+    test('getNumbers()는 방어적 복사를 제공한다.', () => {
+        const original = [1, 2, 3, 4, 5, 6];
+        const lotto = new Lotto(original);
+        const copy = lotto.getNumbers();
+
+        // 반환 배열 변경이 내부 상태에 영향을 주면 안 됨
+        copy[0] = 99;
+        expect(lotto.getNumbers()).toEqual(original);
+    });
 });
